eb2483f12386600b44d7e464a9325f61b284109f
Jacob-Waag <jwaag@luc.edu>
2025-10-08 20:17:02 -0500

    Update keyboard_poll.c

--- src/keyboard_poll.c
+++ src/keyboard_poll.c
@@ -1,1 +1,12 @@
+#define PS2_STATUS_PORT 0x64
+#define PS2_DATA_PORT   0x60
 
+// Returns nonzero if output buffer is full (scancode ready)
+int is_output_buffer_full() {
+    return inb(PS2_STATUS_PORT) & 0x01;
+}
+
+// Read scancode from PS/2 data port
+uint8_t read_scancode() {
+    return inb(PS2_DATA_PORT);
+}
